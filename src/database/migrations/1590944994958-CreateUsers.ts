96bbced37ddcbeacc3803184c872c12aaee96e69
Eronbello <eron.bello@gmail.com>
2020-05-31 15:25:36 -0300

    chore: add UserCreateService and User Routes

--- src/database/migrations/1590944994958-CreateUsers.ts
+++ src/database/migrations/1590944994958-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1590944994958 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
